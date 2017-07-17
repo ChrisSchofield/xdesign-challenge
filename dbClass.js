ca6ee926e564fd73ec9ac28bf118b5eacc1d0a28
Chris Schofield <chris.schofield@outlook.com>
2017-07-17 15:40:24 +0100

    Ready for Submission
    I haven't updated this as thoroughly as I should have, however this now has all requested features.

--- dbClass.js
+++ dbClass.js
@@ -7,25 +7,60 @@ class dbClass { // Creating a class in order to manage DB interactions
     this.connection = conn
   }
 
-  getInfo(table,limit,search) { // This is a method to extract data from the DB, this will be used for the front end and API.
+  getInfo(table,search,limit,offset) { // This is a method to extract data from the DB, this will be used for the front end and API.
 
-    this.connection.connect()
-    var query = "SELECT * FROM " + table + (limit ? " LIMIT " + limit : "")
+    if (search && typeof search == "object") var searchString = " WHERE " + Object.keys(search)[0] + " = '" + Object.values(search)[0] + "'"
+    else if (search && typeof search == "string") {
+      var searchString = " WHERE ",
+          propArray = ["model","type","vehicleUsage","manufacturer","license_plate","transmission","fuel_type","colour"]
+      for (var x = 0;x<propArray.length;x++) {
+        searchString += propArray[x] + " LIKE '" + search + "'"
+        if (x<(propArray.length - 1)) searchString += " OR "
+      }
+    }
 
-    this.connection.query(query, (err, res, fields) => {
-      if (err) throw err
-      console.log(res)
-    })
+    var query = "SELECT * FROM " + table + (search ? searchString : "") + (limit ? " LIMIT " + limit : "") + (offset ? " OFFSET " + (offset*6) : "")
+
+    var res = this.connection.query(query)
+
+    return res
 
   }
 
   putInfo(table,object) { // this is a method to place data in the database, after it has been read and formatted from the XML file provided.
 
-    var query = "INSERT INTO " + table + " SET ?"
+    var keys = Object.keys(object);
+    var values = Object.values(object);
 
-    this.connection.query(query,object,(err, res, fields) => {
-      if (err) throw err
-    })
+    var query = "INSERT INTO " + table + " (" + keys.join(", ")
+        query += ") VALUES ('" + values.join("', '") + "')"
+
+    var res = this.connection.query(query)
+
+    return res
+
+  }
+
+  existsInDB(table,object) { // To ensure duplicate data is avoided.
+
+    var keys = Object.keys(object);
+    var values = Object.values(object);
+
+    var string = "";
+
+    for (var x=0;x<keys.length;x++) {
+      if (keys[x] !== "uuid") {
+        string += keys[x] + " = '" + values[x] + "'"
+        if (x<(keys.length-1)) string += " AND " // -1 because of the zero index
+      }
+    }
+
+    var query = "SELECT * FROM " + table + " WHERE " + string
+    // console.log(query)
+
+    var res = this.connection.query(query)
+
+    return res
 
   }
 
--- index.js
+++ index.js
@@ -2,27 +2,51 @@ const exp     = require("express")
 const pug     = require("pug")
 const x2j     = require("xml2js")
 const fs      = require("fs")
-const sql     = require("mysql")
+const sql     = require("promise-mysql")
+const parser  = require("body-parser")
+const session = require('express-session')
+
+// Own requires
 const dbClass = require("./dbClass.js")
 const extras  = require("./additionalFunctions.js")
 
 
 // Important variables to keep in memory
 let xmlParsed
-let connection = new dbClass(
-  sql.createConnection({
-    host      : "127.0.0.1",
-    user      : "xdesign",
-    password  : "PasswordExample",
-    database  : "xdesign"
-  })
-)
+let vehicles
+let customers
+let connection
+let api = exp()
+let port = 8080
+let router = exp.Router()
+let authSession = []
+
+sql.createConnection({
+  host      : "127.0.0.1",
+  user      : "xdesign",
+  password  : "PasswordExample",
+  database  : "xdesign"
+}).then((conn)=>{
+  connection = new dbClass(conn)
+  if (fs.existsSync("./tmp/clientdata.xml")) {
+    readLocalFile("./tmp/clientdata.xml")
+    fs.unlink("./tmp/clientdata.xml",() => {
+      console.log("Client XML Removed") // Let's tidy up, so that if the server is run again, the same data is not added twice.
+    })
+  }
+  // Once we have a connection, check for XML, and if exists, read.
+})
 
 function readLocalFile(xmlData) {
   fs.readFile(xmlData, 'utf8', (err,xml) => { // Load the XML file to 'xml' variable. utf8 is required or the file will read as bytes.
     if (err) throw err // throw an error in the case the parser can't digest the XML.
+
     populate(xml)
   })
+}
+
+function readString(xmlData) {
+  populate(xml)
 }
 
 function populate(xml) {
@@ -54,6 +78,11 @@ function populate(xml) {
           delete ctx[property]
         }
 
+        if (property == "usage") { // this name clashes with mySQL
+          ctx.vehicleUsage = ctx[property]
+          delete ctx[property]
+        }
+
         if (property == "has_gps" || property == "is_hgv") ctx[property] = (ctx[property] == "true" ? 1 : 0)
 
       }
@@ -63,23 +92,151 @@ function populate(xml) {
       owner.uuid = uniqueIdentifier
       ctx.owner = uniqueIdentifier
 
-      console.log("Car with registration " + ctx.license_plate + " has been populated into the database.")
-      console.log("Customer " + owner.name + " has been populated into the database.")
-      console.log("---")
+      connection.putInfo("customers",owner).then((response) => {
+        // console.log(response)
+      })
 
-      connection.putInfo("customers",owner)
-      connection.putInfo("vehicles",ctx)
+      connection.putInfo("vehicles",ctx).then((response) => {
+        // console.log(response)
+      })
 
       if (vehicle == (xmlParsed.length - 1)) {
         console.log("All vehicles scanned.")
       }
-
     }
+
   })
 }
 
-readLocalFile("./clientdata.xml")
+// API STUFF
 
-// TODO Check DB for duplicate entries.
-// TODO Start building API with Express + Router
-// TODO Build the front-end in Semantic UI + Express + PUG
+api.set('trust proxy', 1)
+api.use(parser.urlencoded({ extended: true }))
+api.use(parser.json())
+api.use(
+  session({
+    secret: 'PasswordExample',
+    resave: true,
+    saveUninitialized: true,
+    cookie: {}
+  })
+)
+
+router.use(function(req, res, next) {
+    console.log('An API function has been called')
+    console.log(req.sessionID)
+    if (req.url == "/auth") next()
+    else if (authSession.includes(req.sessionID)) next()
+    else res.json({ message: 'Not authorised!' })
+});
+
+router.get('/', function(req, res) {
+    res.json({ message: 'Please choose a function. Your available endpoints are /vehicles, /vehicles/:id, /customers, /customers/:id.' })
+})
+
+router.route('/auth') // Auth function
+  .post((req,res) => {
+    // I could integrate a token based auth system here with crypto functions, etc. but for now...
+    var user = "xDesign",
+        pass = "PasswordExample"
+
+    if (req.body.user == user && req.body.pass == pass) {
+      res.json({ message: 'Authorised!' })
+      authSession.push(req.sessionID); // Add session ID to array of allowed sessions
+    } else res.json({ message: 'Not authorised!' }) // deny access if not
+
+  })
+
+//
+// API FUNCTIONS
+//
+
+router.route('/vehicles') // GET all vehicles
+  .get((req,res) => {
+    connection.getInfo("vehicles").then((response) => {
+      res.json(response);
+    })
+  })
+
+router.route('/vehicles/:id') // GET specific vehicle
+  .get((req,res) => {
+    connection.getInfo("vehicles",{id:req.params.id}).then((response) => {
+      res.json(response[0]);
+    })
+  })
+
+router.route('/customers') // GET ALL customers
+  .get((req,res) => {
+    connection.getInfo("customers").then((response) => {
+      res.json(response);
+    })
+  })
+
+router.route('/customers/:id') // GET specific customer
+  .get((req,res) => {
+    connection.getInfo("customers",{id:req.params.id}).then((response) => {
+      res.json(response[0]);
+    })
+  })
+
+// API ATTATCH ROUTER & LISTEN
+
+api.use('/api', router) // Attatch API router
+api.set('view engine', 'pug') // set renderer to PUG
+
+api.use('/public', exp.static('semantic/dist'))
+
+api.get('/', function (req, res) { // FRONT END
+  var offset = (req.query.p ? req.query.p : null)  // check if should be offset
+  var searchQ = (req.query.s ? req.query.s : null) // check if should be Searched
+  connection.getInfo("vehicles",searchQ,6,offset).then((vehicleResponse) => { // Promises required due to async call (Node don't do Sync)
+    connection.getInfo("customers",null,6,offset).then((customerResponse) => {
+      connection.getInfo("vehicles",searchQ).then((vehicleResponseAll) => {
+        res.render('index', {
+          auth: authSession.includes(req.sessionID),
+          title: 'Hey',
+          message: 'Hello there!',
+          vehicles: vehicleResponse,
+          customers: customerResponse,
+          offset: offset,
+          total: Math.ceil(vehicleResponseAll.length / 6),
+          search: searchQ
+        })
+      })
+    })
+  })
+}).post('/', function (req, res) {
+  var user = "xDesign",
+      pass = "PasswordExample"
+
+  if (req.body.user == user && req.body.pass == pass) { // Auth shares credentials with API
+    authSession.push(req.sessionID); // Add session ID to array of allowed sessions
+    var error = null
+  } else {
+    var error = "Failed to log in!"
+  }
+
+  var offset = (req.query.p ? req.query.p : null) // check if should be offset
+  var searchQ = (req.query.s ? req.query.s : null) // check if should be Searched
+  connection.getInfo("vehicles",searchQ,6,offset).then((vehicleResponse) => {
+    connection.getInfo("customers",null,6,offset).then((customerResponse) => {
+      connection.getInfo("vehicles",searchQ).then((vehicleResponseAll) => {
+        res.render('index', {
+          auth: authSession.includes(req.sessionID),
+          title: 'Hey',
+          message: 'Hello there!',
+          vehicles: vehicleResponse,
+          customers: customerResponse,
+          error: error,
+          offset: offset,
+          total: Math.ceil(vehicleResponseAll.length / 6),
+          search: searchQ
+        })
+      })
+    })
+  })
+
+})
+
+api.listen(port)
+console.log('Listening: ' + port)
